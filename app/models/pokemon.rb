c84937a15a24fdc8b1aaf06b3f63d882dac2d483
eseo <edwardhwseo@gmail.com>
2023-09-29 14:42:11 -0500

    Resolved associations between parent and joiner PokemonType models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,7 @@
 class Pokemon < ApplicationRecord
+    has_many :pokemon_types
+    has_many :types, through: :pokemon_types
+
     validates :id, :name, presence: true, uniqueness: true
     validates :hp, :attack, :defense, :sp_attack, :sp_defense, :speed, presence: true
 end
