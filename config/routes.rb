0cfea87af110431a3b53e7d97f3870569e7d33a6
eseo <edwardhwseo@gmail.com>
2023-10-01 17:37:09 -0500

    Update routes.rb
    Changed root route to hhome index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'moves/show'
   # get 'pokemons/index'
   # get 'pokemons/show'
-
+  root to: 'home#index'
   resources :pokemons, only: %i[index show]
   resources :moves, only: %i[index show]
 end
