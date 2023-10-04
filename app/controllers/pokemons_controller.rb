ac5be937ff5f1323b3426366eaeb8185f4fc8f5e
KyunghyunKimRRC <121976813+KyunghyunKimAceProject@users.noreply.github.com>
2023-10-04 12:56:22 -0500

    pokemon show control and html
    Added data from db in controller and displayed them in the html

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,5 +5,8 @@ class PokemonsController < ApplicationController
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    type = Type.find(PokemonType.find_by(pokemon_id: params[:id]).type_id)
+    @type_name = type.name
+    @moves = Move.where(type_id: type.id)
   end
 end
