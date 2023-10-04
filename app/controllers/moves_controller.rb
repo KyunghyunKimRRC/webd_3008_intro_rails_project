077bf7c8012ed091a2885cc7f4d27ecfb34e5114
KyunghyunKimRRC <121976813+KyunghyunKimAceProject@users.noreply.github.com>
2023-10-04 12:57:16 -0500

    move show control and html
    Added data from db to moves controller and displayed data in movie show. Move index is not implemented.

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,7 +1,10 @@
 class MovesController < ApplicationController
   def index
+    @moves = Move.all
   end
 
   def show
+    @move = Move.find(params[:id])
+    @type = Type.find(@move.type_id)
   end
 end
