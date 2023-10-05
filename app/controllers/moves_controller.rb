9a1e829c4a9341a37a02f060976f5f0db3e30228
KyunghyunKimRRC <121976813+KyunghyunKimAceProject@users.noreply.github.com>
2023-10-05 14:02:11 -0500

    Update moves_controller.rb
    made query for required data for show page

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -6,5 +6,6 @@ class MovesController < ApplicationController
   def show
     @move = Move.find(params[:id])
     @type = Type.find(@move.type_id)
+    @pokemons = Pokemon.joins(:types).where(types: {name: @type.name})
   end
 end
